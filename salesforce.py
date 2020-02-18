5ed6aaf77ef54f005791090e81e280c0cc022906
Nirgal Vourgère <nirgal@greenpeace.fr>
2020-02-18 11:24:03 +0100

    Revert "Simplify wanted SF API version"
    This reverts commit c27bb6398f7c9b514595b1de7bd64b2b7576d70c.

--- salesforce.py
+++ salesforce.py
@@ -1,4 +1,4 @@
-from config import CREDIDENTIALS
+from config import CREDIDENTIALS, SF_API_VERSION
 from salesforce_bulk import SalesforceBulk
 from simple_salesforce import Salesforce
 
@@ -6,6 +6,7 @@ from simple_salesforce import Salesforce
 def get_Salesforce():
     params = {}
     params.update(CREDIDENTIALS)
+    params['version'] = SF_API_VERSION
     return Salesforce(**params)
 
 
