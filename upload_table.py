23288e74899879ca7412aab37388eb649d6fc5fa
Nirgal Vourgère <nirgal@greenpeace.fr>
2020-03-10 09:33:05 +0100

    Cut csv files in chunks before uploading

--- upload_table.py
+++ upload_table.py
@@ -2,6 +2,7 @@
 
 import argparse
 import csv
+import io
 import logging
 import sys
 
@@ -9,44 +10,71 @@ import config
 from salesforce import get_SalesforceBulk
 from tabledesc import TableDesc
 
-def csv_split(csvfilename, max_size=1000000):
+
+csvdialect = {
+    'delimiter': ',',
+    'doublequote': True,
+    'escapechar': None,
+    'lineterminator': '\n',
+    'quotechar': '"',
+    'quoting': csv.QUOTE_MINIMAL,
+    'skipinitialspace': False,
+    'strict': True,
+}
+
+def csv_reader(csvfilename):
+    """
+    Takes a postgresql csv file (commas, headers, escape " as "")
+    Yields lines, as string (not list)
+    """
+    with open(csvfilename) as csvfile:
+        reader = csv.reader(csvfile, **csvdialect)
+        for line in reader:
+            buf = io.StringIO()
+            writer = csv.writer(buf, **csvdialect)
+            writer.writerow(line)
+            yield buf.getvalue()
+
+def csv_split(csvfilename, max_size=10000000, max_records=10000):
     """
     Takes a postgresql csv file (commas, headers, escape " as "")
     Yield readable StringIOs with the same format, and a maximum size
     """
-    dialect = {
-        'delimiter': ',',
-        'doublequote': True,
-        'escapechar': None,
-        'lineterminator': '\n',
-        'quotechar': '"',
-        'quoting': csv.QUOTE_MINIMAL,
-        'skipinitialspace': False,
-        'strict': True,
-    }
-    csvfile = open(csvfilename)
-    reader = csv.reader(csvfile, **dialect)
-    #writer = csv.writer(
-    lines = 0
-    for line in reader:
-        lines += 1
-        print(line)
-    print("Found", lines, "lines", file=sys.stderr)
+    logger = logging.getLogger(__name__)
+
+    headers = None
+    buff = ''
+
+    for line in csv_reader(csvfilename):
+        if headers is None:
+            headers = line
+            buff = headers
+            chunk_nb_lines = 1
+            continue
+        if (chunk_nb_lines >= max_records
+           or len(buff) + len(line) >= max_size):
+            logger.debug("Chunk with %s bytes, %s lines", len(buff), chunk_nb_lines)
+            yield io.StringIO(buff)
+            buff = headers
+            chunk_nb_lines = 1
+        buff += line
+        chunk_nb_lines += 1
+    logger.debug("Chunk with %s bytes, %s lines", len(buff), chunk_nb_lines)
+    yield io.StringIO(buff)
 
 
 def upload_csv(tabledesc, csvfilename):
+    logger = logging.getLogger(__name__)
+
     bulk = get_SalesforceBulk()
     jobid = bulk.create_update_job(tabledesc.name, contentType='CSV')
 
-    # for....
-    #csvfile = open(csvfilename).read()
-    content = open(csvfilename)
-    batchid = bulk.post_batch(jobid, content)
+    for chunk in csv_split(csvfilename):
+        batchid = bulk.post_batch(jobid, chunk)
+        bulk.wait_for_batch(jobid, batchid)
+        logger.debug("%s", bulk.get_batch_results(batchid))
+
     bulk.close_job(jobid)
-
-    bulk.wait_for_batch(jobid, batchid)
-
-    print(bulk.get_batch_results(batchid), file=sys.stderr)
 
 
 if __name__ == '__main__':
@@ -71,6 +99,10 @@ if __name__ == '__main__':
     td = TableDesc(args.sftable)
     csvfilename = args.csvfile
 
-    #csv_split(csvfilename)
+    #for chunk in csv_split(csvfilename):
+    #    data = chunk.read()
+    #    print('Chunk starts')
+    #    print(data, end='')
+    #    print('Chunk ends')
 
     upload_csv(td, csvfilename)
