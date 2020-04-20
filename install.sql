bdbfeebdd8e1be190fc912bac5fd0bd6c25d98a1
Nirgal Vourgère <nirgal@greenpeace.fr>
2020-04-20 21:43:16 +0200

    Make sure sync.tablename is UNIQUE

--- install.sql
+++ install.sql
@@ -5,6 +5,6 @@ CREATE SCHEMA salesforce;
 CREATE SCHEMA sync;
 CREATE TABLE sync.status (
 	id SERIAL PRIMARY KEY,
-	tablename VARCHAR(128),
+	tablename VARCHAR(128) UNIQUE,
 	syncuntil TIMESTAMP WITHOUT TIME ZONE  -- UTC
 );
