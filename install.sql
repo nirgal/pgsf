64d3c01f8c6540e3df9305374ea5a2a20d3c5dde
Nirgal Vourgère <nirgal@greenpeace.fr>
2020-11-07 14:43:27 +0100

    Use tablename as primary key
    Migration howto:
    alter table sync.status drop column id;
    alter table sync.status drop constraint status_tablename_key;
    alter table sync.status add constraint status_pkey primary key(tablename);

--- install.sql
+++ install.sql
@@ -4,7 +4,6 @@ CREATE SCHEMA salesforce;
 -- Schema with synchronisation information (for internal use)
 CREATE SCHEMA sync;
 CREATE TABLE sync.status (
-	id SERIAL PRIMARY KEY,
-	tablename VARCHAR(128) UNIQUE,
-	syncuntil TIMESTAMP WITHOUT TIME ZONE  -- UTC
+	tablename VARCHAR(128) PRIMARY KEY,
+	syncuntil TIMESTAMP  -- UTC
 );
