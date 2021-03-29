6d18b35399fe91b8a23ee6fa0aae6b7350522060
Nirgal Vourgère <nirgal@greenpeace.fr>
2021-03-29 18:47:13 +0200

    Moved config to ~/.pgsf

---
+++ config.py
@@ -0,0 +1,36 @@
+import configparser
+from os.path import expanduser
+
+
+def get_config():
+    cfg = configparser.ConfigParser(interpolation=None)
+    path = expanduser('~/.pgsf')
+    assert cfg.read(path), "Could not read " + path
+    return cfg
+
+
+__cfg = get_config()
+
+CREDIDENTIALS = {}
+CREDIDENTIALS['username'] = __cfg['salesforce']['username']
+CREDIDENTIALS['password'] = __cfg['salesforce']['password']
+CREDIDENTIALS['security_token'] = __cfg['salesforce']['security_token']
+CREDIDENTIALS['domain'] = __cfg['salesforce']['domain']
+
+SF_API_VERSION = __cfg['salesforce']['api_version']
+
+DB_HOST = __cfg['postgresql'].get('host', None)
+DB_PORT = __cfg['postgresql'].get('port', None)
+DB_USER = __cfg['postgresql'].get('user', None)
+DB_PASSWORD = __cfg['postgresql'].get('password', None)
+DB_NAME = __cfg['postgresql'].get('db', None)
+
+DB_SCHEMA = __cfg['postgresql'].get('schema', None)
+
+DB_QUOTE_NAMES = __cfg['postgresql'].getboolean('quote_name', False)
+
+JOB_DIR = __cfg['DEFAULT']['job_dir']
+
+LOGFILE = __cfg['DEFAULT']['log_file']
+LOGFORMAT = __cfg['DEFAULT']['log_format']
+LOGLEVEL = __cfg['DEFAULT'].getint('log_level')
