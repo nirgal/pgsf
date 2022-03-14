4defe0378f2f715ed646bc717634885ed21c6356
Nirgal <nirgal@greenpeace.fr>
2022-03-14 15:33:40 +0100

    Remove useless comment

--- tabledesc.py
+++ tabledesc.py
@@ -64,8 +64,6 @@ class TableDesc:
             self.__fields_cache = OrderedDict()
             # First get the info from sf_desc
             for sf_field_info in self.sf_desc['fields']:
-                # if sf_field_info['name'] == 'ChannelProgramName':
-                #     print('NNN1:', sf_field_info)
                 self.__fields_cache[sf_field_info['name']] = sf_field_info
             # Then the the IsIndexed from table FieldDefinition
             sf_definition = self.sf_field_definition
