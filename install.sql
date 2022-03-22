91c34d892d20f873006f59ab6519ef090929ff9e
Nirgal <nirgal@greenpeace.fr>
2022-03-22 18:52:47 +0100

    Added comment in the DB

--- install.sql
+++ install.sql
@@ -3,10 +3,14 @@ create schema salesforce;
 
 create type salesforce.jobstatus as enum ('ready', 'running', 'error');
 
+-- see comments bellow
 create table salesforce.__sync (
-	tablename varchar(70) primary key,  -- see EntityDefinition.QualifiedApiName
-	syncuntil timestamp,  -- UTC
+	tablename varchar(70) primary key,
+	syncuntil timestamp,
 	refresh_minutes int default 10,
 	last_refresh timestamp,
 	status salesforce.jobstatus not null default 'ready'
 );
+comment on column salesforce.__sync.tablename is 'From SF EntityDefinition.QualifiedApiName';
+comment on column salesforce.__sync.syncuntil is 'UTC';
+comment on column salesforce.__sync.last_refresh is 'Local time';
