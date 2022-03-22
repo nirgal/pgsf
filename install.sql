975e4ee3f04be92c130ef98070e8ec1004fcfcb9
Nirgal <nirgal@greenpeace.fr>
2022-03-22 13:00:27 +0100

    Pack __sync sql updates in create table

--- install.sql
+++ install.sql
@@ -1,13 +1,12 @@
 -- Schema where the Salesforce data will be replicated
 create schema salesforce;
 
+create type salesforce.jobstatus as enum ('ready', 'running', 'error');
+
 create table salesforce.__sync (
 	tablename varchar(70) primary key,  -- see EntityDefinition.QualifiedApiName
-	syncuntil timestamp  -- UTC
+	syncuntil timestamp,  -- UTC
+	refresh_minutes int default 10,
+	last_refresh timestamp,
+	status salesforce.jobstatus not null default 'ready'
 );
-
-alter table salesforce.__sync add column refresh_minutes int default 10;
-alter table salesforce.__sync add column last_refresh timestamp;
-
-create type salesforce.jobstatus as enum ('ready', 'running', 'error');
-alter table salesforce.__sync add column status salesforce.jobstatus not null default 'ready';
